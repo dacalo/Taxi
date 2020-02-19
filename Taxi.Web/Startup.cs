413717f2b56f12a81494a37b2686e1049b670566
dacalo <dacalo.soporte@gmail.com>
2020-02-19 13:54:42 -0600

    Part 15 - Publish Azure

--- Taxi.Web/Startup.cs
+++ Taxi.Web/Startup.cs
@@ -42,6 +42,7 @@ namespace Taxi.Web
             
             services.AddTransient<SeedDb>();
             services.AddScoped<IUserHelper, UserHelper>();
+            services.AddScoped<IConverterHelper, ConverterHelper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
